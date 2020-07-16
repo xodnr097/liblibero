06a10bfe2466d4b73628182a30e2716e4c6c2b10
user <user@DESKTOP-HOTABAH>
2020-07-16 19:03:13 +0900

    윤세 : Buy.java , BuyMapper.xml 약간 수정

--- libLIBERO/src/main/java/com/libero/service/domain/Buy.java
+++ libLIBERO/src/main/java/com/libero/service/domain/Buy.java
@@ -1,12 +1,13 @@
 package com.libero.service.domain;
 
+import java.io.Serializable;
 import java.sql.Date;
 import java.util.List;
 
 import lombok.Data;
 
 @Data
-public class Buy {
+public class Buy implements Serializable{
 
 	
 	private int buyNo;
@@ -18,7 +19,7 @@ public class Buy {
 	private String receiverAddr;
 	private String receiverPhone;
 	private int actualPrice;
-	private String buyStatus; //장바구니에 있는 건지 결제 된 상품인지 알리는 상태
+	private int buyStatus; //장바구니에 있는 건지 결제 된 상품인지 알리는 상태
 	private String cancelReason;
 	private User buyerId; // 회원 이름 , 아이디(이메일) , 구매자 주소 , 휴대폰 번호
 	private List<Product> prodNo; //가져올 상품 정보 ==> 상품 명 , 상품 가격 , 저자 / 창작자 이름 , 총결제가격?(앞단에서 가져온다?), 상품 이미지,
@@ -26,8 +27,10 @@ public class Buy {
 								  //카테고리 ?, 해시태그 ?.( 통계를 낼 때 필요?)
 								  // 리스트 -> 여러가지 상품을 받을 수 있어서 필요 하다 싶음.
 	private List<Publish> publish; //인쇄소 주문 정보 ,product 에 해당하는 상품 하나하나의 정보 받기 필요로 인한 리스트 사용.맞나?
-	private Date regDate;
+	private Date payDate;
 	
+	/////////////////추가/////////////////////
+	private int payNo;
 	
 	
 	
