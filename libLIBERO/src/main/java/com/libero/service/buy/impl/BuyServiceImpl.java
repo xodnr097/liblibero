958be769f2b239f3c44ac85357f508d65e4a6b63
user <user@DESKTOP-HOTABAH>
2020-07-16 19:19:50 +0900

    윤세 : BuyServiceImpl 서비스@ 추가

--- libLIBERO/src/main/java/com/libero/service/buy/impl/BuyServiceImpl.java
+++ libLIBERO/src/main/java/com/libero/service/buy/impl/BuyServiceImpl.java
@@ -1,6 +1,11 @@
 package com.libero.service.buy.impl;
 
-public class BuyServiceImpl {
+import org.springframework.stereotype.Service;
+
+import com.libero.service.buy.BuyService;
+
+@Service("buyServiceImpl")
+public class BuyServiceImpl implements BuyService{
 
 	public BuyServiceImpl() {
 		// TODO Auto-generated constructor stub
